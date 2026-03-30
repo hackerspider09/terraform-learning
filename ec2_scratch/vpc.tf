1ff381a6e00fb25473edb2ab406697201367331c
hackerspider09 <hackerspider09@gmail.com>
2026-03-30 20:08:14 +0530

    chore: sg, ec2 added

---
+++ ec2_scratch/vpc.tf
@@ -0,0 +1,87 @@
+
+
+# VPC
+resource "aws_vpc" "terra_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Terra_vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "terra_sn" {
+  vpc_id     = aws_vpc.terra_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Terra_pub_sn"
+  }
+}
+
+# Route table
+resource "aws_route_table" "terra_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/24"
+    gateway_id = aws_internet_gateway.terra_igw.id
+  }
+
+  tags = {
+    Name = "Terra_rt"
+  }
+}
+
+# Route table assosiation
+resource "aws_route_table_association" "rt_ass_sn" {
+  subnet_id      = aws_subnet.terra_sn.id
+  route_table_id = aws_route_table.terra_rt.id
+}
+
+# IG
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  tags = {
+    Name = "Terra_igw"
+  }
+}
+
+# Attach IG to vpc
+resource "aws_internet_gateway_attachment" "terra_igw_attch" {
+  internet_gateway_id = aws_internet_gateway.terra_igw.id
+  vpc_id              = aws_vpc.terra_vpc.id
+}
+
+# Security group
+resource "aws_security_group" "terra_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.terra_vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.terra_sg.id
+  cidr_ipv4         = aws_vpc.terra_vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.terra_sg.id
+  cidr_ipv4         = aws_vpc.terra_vpc.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.terra_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
